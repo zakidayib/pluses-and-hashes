3c247d61d9f94b2e4049203d9182b125b4ec1d64
zaki tayyip <zakidayib1997@gmail.com>
2023-06-12 22:12:40 +0300

    plushash- solved

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 // Good Luck 💪🏾
+
+function plusHashCount(str){
+    let plusCount = 0;
+    let hashCount = 0;
+
+    for(let i=0; i<str.length; i++){
+        if(str[i] === "#"){
+            hashCount++;
+        }
+
+        if(str[i] === "+"){
+            plusCount++;
+        }
+    }
+
+    return [plusCount, hashCount]
+}
+
+console.log(plusHashCount("#+++#+#++#"));
